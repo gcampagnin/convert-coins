e36ad8cc746de4969354ab94f95efa684feac91a
Gustavo Campagnin <gcampagnin@gmail.com>
2024-05-08 10:17:08 -0300

    get currency

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+// Obtendo elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
-// Manipulando o input amount para receber somente números
+// Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
 	const hasCharactersRegex = /\D+/g
 	amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturando evento de submit do formulário.
+form.onsubmit = (event) => {
+	event.preventDefault()
+
+
+}
